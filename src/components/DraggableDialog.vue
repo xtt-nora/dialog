bdaa21ca08f0c923a16e62133b6b20e2ece77d8f
nora <100661009+xtt-nora@users.noreply.github.com>
2024-02-02 11:08:24 +0800

    Update DraggableDialog.vue
    弹窗优化，自定义footer，将方法暴露出去

--- src/components/DraggableDialog.vue
+++ src/components/DraggableDialog.vue
@@ -1,122 +1,177 @@
 <template>
   <teleport to="body">
-  <vue-drag-resize 
-  :parentLimitation="false"
-  :isActive="true" 
-  :isResizable="true" 
-  :w="500" 
-  :h="400" 
-  :z="zIndex1"
-  v-on:resizing="resize" 
-  v-on:dragging="resize"
-  @mousedown="onActivated"
-   v-if="info"
-   >
-    <div class="dialog-header">
-      <slot name="header">新建弹窗</slot>
-      <n-button size="tiny" @click="closeDialog">Close</n-button>
-    </div> 
-    <div class="dialog-content">
-      <slot></slot>
-    </div>
-    <div class="dialog-footer">
-      <n-button size="tiny" @click="submit">确定</n-button>
+<vue-drag-resize 
+:parentLimitation="true"
+:isActive="isActive"
+:isResizable="true"
+:w="700"
+:h="600"
+:minw="200"
+:minh="300"
+:x="initX"
+:y="initY"
+:z="zIndex1"
+v-on:resizing="resize"
+v-on:dragging="resize"
+@mousedown.stop="onActivated"
+ v-if="info"
+ >
+  <n-el tag="div" class="dialog-header">
+    <slot name="header">新建弹窗</slot>
+    <n-el tag="div">
+      <n-button size="tiny" class="blub">
+    <template #icon>
+      <n-icon><bulb-icon /></n-icon>
+    </template>
+  </n-button>
+  <n-button size="tiny"  @click="closeDialog">
+    <template #icon>
+      <n-icon><close-icon /></n-icon>
+    </template>
+  </n-button>
+    </n-el>
+  </n-el>
+  <n-el  tag="div" class="dialog-content" @mousedown.stop="startDrag">
+    <slot></slot>
+  </n-el>
+  <n-el  tag="div" class="dialog-footer"  @mousedown.stop="startDrag">
+    <slot name="button">
+      <n-button size="tiny" @click="submit" class="submit">确定</n-button>
       <n-button size="tiny" @click="closeDialog">关闭</n-button>   
-    </div>
-  </vue-drag-resize>
-    </teleport>
+     </slot>
+    <!-- <n-button size="tiny" @click="submit" class="submit">确定</n-button>
+    <n-button size="tiny" @click="closeDialog">关闭</n-button>    -->
+  </n-el>
+</vue-drag-resize>
+</teleport>
 </template>
 
 <script setup lang="ts">
 import { ref, toRefs, defineProps, defineEmits, onMounted  } from 'vue';
+//@ts-ignore
 import VueDragResize from 'vue-drag-resize/src';
 import { useGlobalStore } from '@/stores/modules/global';
+import { CloseOutline as CloseIcon, BulbOutline as BulbIcon } from '@vicons/ionicons5'
 const globalStore = useGlobalStore()
 const props = defineProps({
-  info: Boolean,
+info: Boolean,
 })
+const initX = ref(0)
+const initY = ref(0)
 const {info} =toRefs(props)
-const zIndex1 = ref<number>(9999)
+const isActive = ref(true)
+const zIndex1 = ref<number>(9)
 const emit = defineEmits(['closeDialog','submit'])
-const closeDialog= () =>{
-  let param={
-    info: false
-  }
-  emit('closeDialog',param)
+const   closeDialog= () =>{
+let param={
+  info: false
+}
+emit('closeDialog',param)
 }
 const submit = ()=>{
-  let param={
-    info: false
-  }
-  emit('submit',param)
+let param={
+  info: false
 }
-const width = ref(0); 
-const height = ref(0); 
-const top = ref(0); 
+emit('submit',param)
+}
+const width = ref(0);
+const height = ref(0);
+const top = ref(0);
 const left = ref(0);
 function resize (newRect:any){
-  width.value = newRect.width;
-  height.value = newRect.height;
-  top.value = newRect.top;
-  left.value = newRect.left;
+width.value = newRect.width;
+height.value = newRect.height;
+top.value = newRect.top;
+left.value = newRect.left;
 }
 onMounted(() => {
+  const app = document.getElementById('app')
+  initX.value = app?.offsetWidth/4
+  initY.value = app?.offsetHeight/6
 })
 const onActivated =() =>{
-  globalStore.$patch((state: { globalZIndex: number; }) => { 
-      state.globalZIndex++
-  })
-  zIndex1.value = globalStore.globalZIndex
+globalStore.$patch((state: { globalZIndex: number; }) => {
+    state.globalZIndex++
+})
+zIndex1.value = globalStore.globalZIndex
+}
+const startDrag =()=>{
+  isActive.value  = false
 }
 </script>
 
 <style lang="scss" scoped>
-::v-deep .content-container{
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  height:100%;
+:deep(.content-container){
+display: flex;
+flex-direction: column;
+position: relative;
+height:100%;
+border-radius: 10px;
+background-color: #FFFFFF;
 }
 .dialog-header {
- background-color: $primary-color;
-  color: white;
-  padding: 10px;
-  cursor: move;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+background-color: var(--tab-color);
+border: 1px solid var(--border-color);
+border-bottom:none;
+padding: 10px;
+height: 5%;
+cursor: move;
+// color: #818594;
+color: var(--text-color-base);
+border-radius: 10px 10px 0 0;
+display: flex;
+justify-content: space-between;
+align-items: center;
+.blub{
+  margin-right: 10px;
+}
 }
 
 .dialog-content {
-  background-color: white;
-  padding: 20px;
-  flex:1;
-  overflow: auto;
+background-color: var(--body-color);
+border: 1px solid var(--border-color);
+border:1px solid var(--border-color); 
+border-top:none;
+border-bottom:none;
+padding: 20px;
+height: 85%;
+overflow: auto;
 }
 .dialog-footer{
-  height: 23px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
+height: 10%;
+padding-right: 20px;
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+border-radius: 0 0 10px 10px;
+background-color: var(--body-color);
+border: 1px solid var(--border-color);
+border-top:none;
+.submit{
+  margin-right: 10px;
+}
 }
 .vdr.active:before {
-  display:none;
+display:none;
 }
 .vdr.active{
-  border: 1px solid;
-  position: fixed;
+border: 1px solid var(--border-color);
+border-radius: 10px;
+position: fixed;
 }
-::v-deep .vdr-stick {
-    box-sizing: border-box;
-    position: absolute;
-    font-size: 1px;
-    background: transparent;
-    border: 1px solid transparent;
-    box-shadow: 0 0 0.10417vw transparent;
+:deep(.vdr-stick) {
+  box-sizing: border-box;
+  position: absolute;
+  font-size: 1px;
+  background: transparent;
+  border: 1px solid transparent;
+  box-shadow: 0 0 0.10417vw transparent;
 }
+
 .carousel-img {
-  width: 100%;
-  height: 240px;
-  object-fit: cover;
+width: 100%;
+height: 240px;
+object-fit: cover;
 }
 </style>
